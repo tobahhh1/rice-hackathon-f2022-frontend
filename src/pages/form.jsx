67788c26dc26a5334d5d26fef588585ed838e9eb
tobahhh1 <manningunger@gmail.com>
2022-09-24 11:45:40 -0500

    fixed router

--- src/pages/form.jsx
+++ src/pages/form.jsx
@@ -1,78 +1,56 @@
-import logo from './logo.svg';
-import './App.css';
-import React, {useState} from 'react';
-import { Box } from '@chakra-ui/react';
-import {
-  FormControl,
-  FormLabel,
-  FormErrorMessage,
-  FormHelperText,
-} from '@chakra-ui/react';
-import { Wrap, WrapItem } from '@chakra-ui/react';
-
-
+import React, { useState } from "react";
+// import { Box } from "@chakra-ui/react";
+// import {
+//   FormControl,
+//   FormLabel,
+//   FormErrorMessage,
+//   FormHelperText,
+// } from "@chakra-ui/react";
+// import { Wrap, WrapItem } from "@chakra-ui/react";
 
 function FormPage(props) {
-  const[destinations,setDestinations] = useState([]);
-  const [number, setNumber] = useState(0);
-  const maxDestinations = 5;
+  // const [destinations, setDestinations] = useState([]);
+  // const [number, setNumber] = useState(0);
+  // const maxDestinations = 5;
 
+  // const handleSubmit = (e) => {
+  //   const newform = new FormData();
+  //   formData.append("Destinations", destinations);
+  //   e.preventDefault();
+  // };
 
-  const handleSubmit  = (e) => {
-    const newform = new FormData ();
-    formData.append('Destinations', destinations);
-    e.preventDefault();
+  // const handleNext = (e) => {
+  //   if (number < maxDestinations) {
+  //     setDestinations(destinations.add(e.target.value));
+  //     setNumber(number + 1);
+  //   } else {
+  //     e.preventDefault();
+  //   }
+  // };
 
+  // const deleteDestination = (e) => {
+  //   setDestinations(destinations.remove(e.target.value));
+  // };
 
-  }
+  // return (
+  //   <div className="App">
+  //   <Form onSubmit = {handleSubmit}>
+  //     {
+  //       destinations.map((destination) =>
 
-  const handleNext = (e) => {
-    if ( number < maxDestinations){
-      setDestinations(destinations.add(e.target.value));
-      setNumber(number+1);
+  //     )}
 
-    }
-    else{
-      e.preventDefault();
-    }
-    
-    
+  //       <Box>
+  //         <label>
 
-
-
-  }
-
-  const deleteDestination = (e) => {
-    setDestinations(destinations.remove(e.target.value));
-
-  }
-
-
-
-
-
-  return (
-    <div className="App">
-    <Form onSubmit = {handleSubmit}>
-      {
-        destinations.map((destination) => 
-
-
-      )}
-      
-      
-
-
-        <Box>
-          <label>
-          
-          <input type = "text" onChange= {handleNext} value = {destination}/>
-          </label>
-        </Box>
-    </Form>
-      { % if }
-    </div>
-  );
+  //         <input type = "text" onChange= {handleNext} value = {destination}/>
+  //         </label>
+  //       </Box>
+  //   </Form>
+  //     { % if }
+  //   </div>
+  // );
+  return <div>Broken rn</div>;
 }
 
-export default Formpage;
+export default FormPage;
