7748f1aea90d016bb122822775c87b230e06a248
hemchande <hemchandeisha@gmail.com>
2022-09-24 18:00:49 -0500

    making changes

--- src/pages/form.jsx
+++ src/pages/form.jsx
@@ -1,5 +1,9 @@
 import React, { useState } from "react";
-// import { Box } from "@chakra-ui/react";
+ import { Box } from "@chakra-ui/react";
+
+import { useRef } from "react";
+import Chip  from "@material-ui/core/Chip";
+import ChipInput from "material-ui-chip-input";
 // import {
 //   FormControl,
 //   FormLabel,
@@ -10,39 +14,82 @@ import React, { useState } from "react";
 
 function FormPage(props) {
   const[destinations,setDestinations] = useState([]);
-  const [number, setNumber] = useState(0);
+  const [residence, setResidence] = useState(null);
+  
   const maxDestinations = 5;
+  //Boolean = number < maxDestinations;
 
 
   const handleSubmit  = (e) => {
     const newform = new FormData ();
-    formData.append('Destinations', destinations);
+    newform.add('Destinations', destinations);
     e.preventDefault();
 
 
   }
 
-  
+  const handleChange = (e) => {
 
-  const handleNext = (e) => {
-    if ( number < maxDestinations){
-      setDestinations(destinations.add(e.target.value));
-      setNumber(number+1);
 
-    }
-    else{
-      e.preventDefault();
-    }
-    
+
+  }
+
+  const setDestination = (e) => {
+    const list = destinations.copy();
+    list.add(e.target.value)
+    setDestinations(list);
+
+
+  }
+
+
+
+  const handleResidence = (e) => {
+
+    setResidence(e.target.value);
 
 
   }
 
   const deleteDestination = (e) => {
-    setDestinations(destinations.remove(e.target.value));
+    const list = destinations.copy();
+    list.remove(e.target.value)
+    setDestinations(list);
+
 
   }
 
+  const inputForm = (e) => {
+
+
+  }
+
+  const chipRenderer = ({ chip, className, handleClick, handleDelete }, key) => (
+    <Chip
+      className={className}
+      key={key}
+      label={chip}
+      onClick={handleClick}
+      onDelete={handleDelete}
+      size="small"
+    />
+  );
+
+  let destinationChips = destinations.map((destination, index) => {
+    return (
+    
+        <div>
+            <label>
+            Destination
+            <Chip label = {destination} variant="outlined" onDelete={deleteDestination}>
+
+            </Chip>
+            </label>
+        </div>
+    )
+  }
+  
+  )
 
 
 
@@ -51,36 +98,36 @@ function FormPage(props) {
 
 
 
-  return (
+
+    return (
     <div className="App">
     <form onSubmit = {handleSubmit}>
-      {
-        destinations.map((destination) => (
-
-            <Box>
+        <Box component="div">
             <label>
-            
-            <input type = "text" onChange= {handleNext} value = {destination}/>
+                Residence 
+                <input type = "text"  onChange = {handleResidence} value = {residence}/>
+                
             </label>
           </Box>
+          <Box>
+            <ChipInput
+          chipRenderer={chipRenderer}
+          label="Destinations" 
+          handleChange = {setDestination}
+        />
+            {destinationChips}
+        </Box>
 
-          
-          
+    <button onClick={handleSubmit} variant="outlined" color="secondary">
+      Submit
+    </button>
+    </form >
 
 
-
-        )
-
-
-      )}
-      
-      
-
-
-       
-    </form>
     </div>
   );
-}
+
+    }
+
 
 export default FormPage;
